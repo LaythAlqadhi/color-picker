053f545b19497924176470bb74f231238ee6a793
Layth <125077045+LaythAlqadhi@users.noreply.github.com>
2023-05-04 01:27:39 +0300

    Refactor code and add new features
    This commit refactors the original code and adds new features. The original code defined a function called `colors()` to update the background color of the document based on the values of three input elements with the classes `.red`, `.green`, and `.blue`. The updated code separates concerns by defining two separate functions, `updateColors()` and `copyContent()`, using arrow functions for more concise code. 
    
    `updateColors()` selects the input elements with the classes `.red`, `.green`, and `.blue` using `querySelector()`, then sets the background color of the document and updates the text content of a span element with the id `#rgb`. 
    
    `copyContent()` copies the text content of the span element with the id `#rgb` to the clipboard when a button is clicked, using the `navigator.clipboard` API. Error handling is implemented using a try-catch block. 
    
    The updated code also selects all the input elements with the class `.rangeSliders` using `querySelectorAll()` and adds an event listener to each of them, replacing the individual queries for the red, green, and blue input elements in the original code. 
    
    Overall, this commit improves the modularity and efficiency of the code and adds new features for a better user experience.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-function colors(){
-const red = document.querySelector(".red");
-const green = document.querySelector(".green");
-const blue = document.querySelector(".blue");
-document.body.style.background = `rgb(${red.value},${green.value},${blue.value})`;
-document.querySelector("span").innerHTML = `RGB(${red.value},${green.value},${blue.value})`
-}
+const updateColors = () => {
+  const red = document.querySelector(".red");
+  const green = document.querySelector(".green");
+  const blue = document.querySelector(".blue");
+  const text = document.querySelector("#rgb");
+  const bgColor = `rgb(${red.value},${green.value},${blue.value})`;
+  document.body.style.background = bgColor;
+  text.innerHTML = `rgb(${red.value},${green.value},${blue.value})`;
+};
+
+const copyContent = async () => {
+  const text = document.querySelector("#rgb");
+  try {
+    await navigator.clipboard.writeText(text.innerText);
+    console.log("Content copied to clipboard");
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+};
+
+document.querySelectorAll(".rangeSliders input").forEach((input) => {
+  input.addEventListener("input", () => {
+    updateColors();
+  });
+});
+
+document.querySelector(".btn").addEventListener("click", () => {
+  copyContent();
+});
